3669505701674a712b82fc561e342ad0cf490ae8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-23 23:54:17 +0600

    While loop

---
+++ Module 1/while loop.py
@@ -0,0 +1,15 @@
+# num=int(input())
+# while num<=10:
+#     num+=1
+#     if num==4:
+#         continue
+#     print(num)
+#     # if num==6:
+#         # break
+
+num=int(input())
+while num<=10:
+    num+=1
+    if num%2==1:
+        continue
+    print(num)
