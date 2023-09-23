50c080321cf227f17c5ade85f3baa897821f57ed
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-24 00:18:59 +0600

    for loop

---
+++ Module 1/For_loop.py
@@ -0,0 +1,15 @@
+# # n=int(input())
+# num = [5, 10, 15, 20, 25]
+# sss= ["alauddin",'balouddin','chalouddin']
+# for n in num:
+#     print(n)
+# for n in sss:
+#     print(n)
+# sum=0
+# for s in num:
+#     sum+=s
+    
+# print(sum)
+
+for i in range(1,10,2):
+    print(i)
