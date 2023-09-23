badf8dddc948576ff3069cf93fd234d7a89963b3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-23 22:50:16 +0600

    varibale solved

--- Module 1/variable.py
+++ Module 1/variable.py
@@ -1,6 +1,15 @@
+""" Multple comment alt+shift A """
 age=45
 name='alauddin'
 distric = 'Chittagong'
+sec='c'
 Is_single=True
 is_gumaccy= False
-print(Is_single)
+weight=75.78
+# print(type(Is_single))
+# print(type(weight))
+# print(type(sec))
+# print(type(name))
+# print("Add", 2, "and", 3, "to get ", 5)
+text=f" uddin age {age} living in {distric}. her weight {weight} "
+print(text)
