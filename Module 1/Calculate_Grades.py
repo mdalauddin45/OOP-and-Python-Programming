17d032ee167fb00564a349ead80ba8ac99d82419
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-24 01:13:19 +0600

    Grade system using if else

---
+++ Module 1/Calculate_Grades.py
@@ -0,0 +1,17 @@
+mark=int(input())
+if mark>=90:
+    print("Your Grade is A+")
+elif mark>=80:
+    print("Your Grade is A")
+elif mark>=70:
+    print("Your Grade is B+")
+elif mark>=60:
+    print("Your Grade is B")
+elif mark>=50:
+    print("Your Grade is C")
+elif mark>=33 :
+    print("Your Grade is D")
+elif mark<=32:
+    print("Your Grade is F")
+else:
+    print("Invalid input")
