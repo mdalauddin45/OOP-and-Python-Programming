e71350313dff3c9674e8111928e18af26aa62033
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-23 23:11:04 +0600

    input

---
+++ Module 1/input.py
@@ -0,0 +1,13 @@
+# print("now i need money")
+# input()
+# input("Give me some money : ")
+# money = input("Give me some money: ")
+# print(money)
+fist_monye = input('kodom ali i need some monye taka da taka da: ')
+second_monye = input('Peyara begum apu i need some monye taka daw: ')
+fist_monye_int=int(fist_monye)
+second_monye_int=int(second_monye)
+print('money i got ',fist_monye , " and ",second_monye)
+# total=fist_monye+second_monye
+total=fist_monye_int + second_monye_int
+print('total money i got ',total)
