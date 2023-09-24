2d2d0432b6267500d24d4e8e86496d1e0eac0e6d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-24 22:16:23 +0600

    function

---
+++ Module 2/Function.py
@@ -0,0 +1,22 @@
+def double_it(num):
+    result = num*2
+    print(result)
+    return result
+    
+double_it(8)
+
+def sum(num1,num2):
+    result = num1+num2
+    print(result)
+
+sum(20,39)
+
+def mul(num1,num2):
+    result = num1*num2
+    return result
+
+total = mul(3,4)
+print(total)
+
+t=double_it(total)
+print(t)
