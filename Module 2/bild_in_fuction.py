9b892d47ca9e569e29038e3cfab2586f4895a473
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-24 23:14:22 +0600

    module

---
+++ Module 2/bild_in_fuction.py
@@ -0,0 +1,8 @@
+highest = max(10,20,40,28,40)
+smallest = min(29,34,208,349,5,49,2,1,0)
+total = sum([29,34,208,349,5,49,2,1,0])
+# print(highest)
+# print(smallest)
+count = len([10,20,40,28,40])
+print(count)
+print(total)
---
+++ Module 2/module.py
@@ -0,0 +1,2 @@
+from Function import double_it as dt
+dt(45)
