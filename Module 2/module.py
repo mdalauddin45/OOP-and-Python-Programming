94df268df1bbab51ef165f97fee2a4168a7c6bbd
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-25 22:01:59 +0600

    ses

--- Module 2/module.py
+++ Module 2/module.py
@@ -1,2 +1,9 @@
-from Function import double_it as dt
-dt(45)
+# from Function import double_it as dt
+# dt(45)
+
+def display_person(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}: {value}")
+
+
+display_person(Name="Amir Khan", Age="45")
