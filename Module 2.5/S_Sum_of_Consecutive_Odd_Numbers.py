9097a04319e33532f47551c007a73f3ee4e769c8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-25 23:14:29 +0600

    S. Sum of Consecutive Odd Numbers solved

--- Module 2.5/S_Sum_of_Consecutive_Odd_Numbers.py
+++ Module 2.5/S_Sum_of_Consecutive_Odd_Numbers.py
@@ -1,16 +1,15 @@
 n = int(input())
 
 for _ in range(n):
-    #ak line a input nity holy aivhaby nity hoby
     x, y = map(int, input().split())
     s=0
-    if x+1<y:
-        for num in range(x,y):
+    if x<y:
+        for num in range(x+1,y):
             if num%2==1:
                 s+=num
             
     else:
-        for num in range(y,x):
+        for num in range(y+1,x):
             if num%2==1:
                 s+=num
     print(s)
