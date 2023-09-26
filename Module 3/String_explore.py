2e0aa5868d44ff1ef0ca9fa8a9db2c43afcd1ffe
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-26 20:59:15 +0600

    string

---
+++ Module 3/String_explore.py
@@ -0,0 +1,23 @@
+name='ala\'s uddin' #escape
+name2="Ala uddin "
+name3=""" 
+hello jebu!
+how are you 
+"""
+# print(name)
+#mutable means changeable 
+#unmuteable unchangeable
+
+# for char in name2:
+#     print(char)
+
+
+#reverse
+print(name2[::-1])
+
+if 'udd' in name2:
+    print('acy')
+else:
+    print('nei')
+print(name2.upper())
+print(name2.lower())
