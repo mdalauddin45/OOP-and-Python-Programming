9c67d2e17f94cf5b4f2e77a01387a77d23247280
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-26 23:17:52 +0600

    file

---
+++ Module 3/file.py
@@ -0,0 +1,12 @@
+#.csv comma separated value
+#.txt text file
+
+# with open('message.txt','w') as file:
+#     file.write('i love you, python!')
+
+# with open('message.txt','a') as file:
+#     file.write('i love you, python!')
+
+with open('message.txt','r') as file:
+    text=file.read()
+    print(text)
--- Module 3/tempCodeRunnerFile.py
+++ Module 3/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-Hello world!
+message.txt
