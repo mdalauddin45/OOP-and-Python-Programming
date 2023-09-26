744751d30b620e044b936139e57309325f158994
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-26 22:09:55 +0600

    bild in module

---
+++ Module 3/build_in_module.py
@@ -0,0 +1,15 @@
+from math import *
+from random import *
+from time import *
+from datetime import *
+
+result=ceil(4.6)
+# print(result)
+result1=floor(4.6)
+# print(result1)
+
+# print(random())
+# print(randint(1,100))
+# # sleep(3)
+# print(choice(['ala','sakib','musfiq','tamim']))
+print(datetime.date())
