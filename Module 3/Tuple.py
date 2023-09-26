f6a6e8c4469e27ae45abb20d77054823fc767269
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-26 21:30:44 +0600

    tuple

---
+++ Module 3/Tuple.py
@@ -0,0 +1,19 @@
+def multiple():
+    return 3,4
+# print(multiple())
+
+things='pen','tripod', 'water bottle','helo','chiki'
+# print(type(things))
+# print(things[0])
+# print(things[2:4])
+
+# if 'pen' in things:
+#     print("achi re bhai aci")
+# else:
+#     print('sry')
+# print(len(things)) #length
+mega=([2,3,4,5,6],[67,34,7,83,23])
+# print(type(mega))
+#tuple not asignable
+mega[0][1]=65
+print(mega)
