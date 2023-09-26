3e459c2a06b891f64cab74065a7c5c6989d37b7d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-26 23:11:47 +0600

    try except

---
+++ Module 3/try_except.py
@@ -0,0 +1,9 @@
+try:
+    result=45/5
+except:
+     #only error khaily cholby
+    print('error happened')
+finally:
+    #error khaily o cholby na khaily o cholby
+    print('finally here')
+print('done')
