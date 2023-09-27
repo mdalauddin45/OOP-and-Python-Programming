71bc8d84aa158e56f677a1ee915503d3e343c768
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-27 22:47:47 +0600

    Y. Range sum query

--- Module 3.5/Y_Range_sum_query.py
+++ Module 3.5/Y_Range_sum_query.py
@@ -1,7 +1,9 @@
 n,m=map(int, input().split())
 arr=list(map(int,input().split()))
+s=[0]*(n+1)
+for i in range(1,n+1):
+    s[i]=s[i-1]+arr[i-1]
 for _ in range(m):
-    s=0
     x,y=map(int, input().split())
-    s=sum(arr[x-1:y])
-    print(s)
+    q=s[y]-s[x-1]
+    print(q)
