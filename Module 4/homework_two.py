42feefae1e65fe3dd9be975eb3787632e859af08
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-30 22:40:29 +0600

    homework two

---
+++ Module 4/homework_two.py
@@ -0,0 +1,8 @@
+class Pen:
+    def __init__(self,brand,color,price):
+        self.brand=brand
+        self.color=color
+        self.price=price
+
+my_pen=Pen('metador','black',10)
+print(my_pen.brand,my_pen.color,my_pen.price)
