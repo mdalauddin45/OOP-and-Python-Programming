4081ceb503cccd8f92b29491a188664f54be3dc6
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-30 21:29:59 +0600

    phone py

---
+++ Module 4/phone.py
@@ -0,0 +1,9 @@
+class Phone:
+    price = 190000
+    color = 'black'
+    brand= 'iphone'
+my_phone= Phone()
+print(my_phone) #print addres of phone
+print(my_phone.brand)
+print(my_phone.price)
+print(my_phone.color)
