23ba91353c122d5a7932b6d428a16fda3cdcab1a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-10-04 12:35:57 +0600

    inheritance

---
+++ Module 7/inheritance.py
@@ -0,0 +1,26 @@
+#inheritance provides you "is a" realtion
+class Animal:
+    pass
+
+#Dog is a animal
+class Dog(Animal):
+    pass
+
+#Tiger is a animal
+class Tiger(Animal):
+    pass
+
+class Furniture:
+    pass
+
+#Chir is a Furniture
+class Chir(Furniture):
+    pass
+
+#Tabel is a Furniture
+class Tabel(Furniture):
+    pass
+
+#bed is a Furniture
+class Bed(Furniture):
+    pass
